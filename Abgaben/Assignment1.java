ddc9b4fcea91fafe4ddc9b9ddc67b5bf9e805c63
wacken <sebastianwalchi@gmail.com>
2020-10-10 22:09:14 +0200

    add Abgabe and make sensible git ignore and change GRNVS@git.net.in.tum.de/ to own

---
+++ Abgaben/Assignment1.java
@@ -0,0 +1,37 @@
+/* Do NOT!!! put a package statement here, that would break the build system */
+
+public class Assignment1 {
+
+	public static void run(long max) {
+/*====================================TODO===================================*/
+		for(int i = 1; i <= max; i++)
+		{
+			if(i % 5 == 0 && i % 3 == 0){
+				
+				System.out.println("BuzzFizz");
+				continue;
+			}	
+			if(i % 5 == 0)
+				System.out.println("Buzz");
+			if(i % 3 == 0)
+				System.out.println("Fizz");
+			if(i % 5 != 0 && i % 3 != 0)
+				System.out.println(i);
+		}
+/*===========================================================================*/
+	}
+
+
+
+	public static void main(String[] argv) {
+		//Arguments args = new Arguments(argv);
+		try{
+			run(15);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			System.exit(1);
+		}
+	}
+}
